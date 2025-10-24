cfb4953f564ac1ea8755be2ea46b77549dfdc988
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-10-24 23:49:35 +0000

    feat: home interface setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,32 @@
-export function Home() {}
+import Unit from "../components/UnitDropDown";
+import SearchBar from "../components/SearchBar";
+import CurrentWeather from "../components/CurrentWeather";
+import ForecastCard from "../components/ForecastCard";
+import HourlyForecast from "../components/HourlyForecast";
+
+export function Home() {
+  return (
+    <>
+      <header>
+        <div>
+          <img src="" alt="" />
+          <span>Hey Weather</span>
+          <Unit />
+        </div>
+      </header>
+      <main>
+        <p>How is the sky looking today?</p>
+        <SearchBar />
+        <div>
+          <div>
+            <CurrentWeather />
+            <ForecastCard />
+          </div>
+          <HourlyForecast />
+        </div>
+      </main>
+    </>
+  );
+}
 
 export default Home;
